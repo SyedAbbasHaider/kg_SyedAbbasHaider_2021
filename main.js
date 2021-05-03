7699892f7b30091b9c1999a9c5a4d52a5bdda256
SyedAbbasHaider <syedhaider066@gmail.com>
2021-05-02 22:50:33 -0400

    Final Solution
    Updates map. Removed comments and finalized for submission.

--- main.js
+++ main.js
@@ -1,40 +1,32 @@
-var myarg = process.argv.slice(2);
+var myArg = process.argv.slice(2);
 
-//let text = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-console.log('myarg: ', myarg);
-// console.log('first arg is: ', myarg[0]);
-// console.log(text[0]);
-
-console.log(typeof(myarg[0]));
-
-let length = myarg.length;
-//console.log(length);
-
+var result = [];
 
 
 let map = new Map([
-    ['0', 'zero'],
-    ['1', 'one'],
-    ['2', 'two'],
-    ['3', 'three'],
-    ['4', 'four'],
-    ['5', 'five'],
-    ['6', 'six'],
-    ['7', 'seven'],
-    ['8', 'eight'],
-    ['9', 'nine'],
+    ['0', 'Zero'],
+    ['1', 'One'],
+    ['2', 'Two'],
+    ['3', 'Three'],
+    ['4', 'Four'],
+    ['5', 'Five'],
+    ['6', 'Six'],
+    ['7', 'Seven'],
+    ['8', 'Eight'],
+    ['9', 'Nine'],
 ]);
 
-for (let i = 0; i < myarg.length; i++) {
-    if (map.has(myarg[i])) {
-        if (myarg[i] > 9) {
-            
+for (let i = 0; i < myArg.length; i++) {
+    var myChar = myArg[i].split('');
+    var fResult = "";
+    for (let j = 0; j < myChar.length; j++) {
+        if (map.has(myChar[j])) {
+            var output = map.get(myChar[j]);
+            var fResult = fResult.concat(output) 
         }
-        console.log(map.get(myarg[i]))
     }
+    result.push(fResult);
 }
 
-// if (map.has(myarg[0])) {
-//     console.log(map.get(myarg[0]))
-// }
+ console.log(result.join(', '))
 
